c19618e6ae6e6d3785cd4d2d1936a09eea96664d
Soviskas <kisilovoleksandr48@gmail.com>
2025-02-07 01:00:47 +0200

    start working with JS

---
+++ main.js
@@ -0,0 +1,5 @@
+ document.write("JavaScript say hello!");
+        console.log("JavaScript say hello!");
+        console.info("JavaScript say hello!");
+        console.error("JavaScript say hello!");
+        console.warn("JavaScript say hello!");
