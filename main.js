4f913c36279652ab9a4235cbba5ff0e1189cab7c
Soviskas <kisilovoleksandr48@gmail.com>
2025-03-01 19:47:06 +0200

    Update#1-01.03.2025-19:46

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
- document.write("JavaScript say hello!");
-        console.log("JavaScript say hello!");
-        console.info("JavaScript say hello!");
-        console.error("JavaScript say hello!");
-        console.warn("JavaScript say hello!");
+//  document.write("JavaScript say hello!");
+//         console.log("JavaScript say hello!");
+//         console.info("JavaScript say hello!");
+//         console.error("JavaScript say hello!");
+//         console.warn("JavaScript say hello!");
+
+// var num;
+// num = 10;
+
+var num = 10;
+num = 12;
+console.log("Variable:" + num);
+
+// var number;
+// number = true;
+// number = 8;
+// number = 8.8;
+// number = "Stroka";
+
+ 
