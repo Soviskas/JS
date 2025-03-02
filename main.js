4e65b67c689921903b4593272bf1a4bd5d39b267
Soviskas <kisilovoleksandr48@gmail.com>
2025-03-02 15:38:06 +0200

    update#2-2.03.2025-15:37

--- main.js
+++ main.js
@@ -17,20 +17,49 @@
 // number = 8.8;
 // number = "Stroka";
 
-var num_1 = 5;
-var num_2 = 10;
+// var num_1 = 5;
+// var num_2 = 10;
 // var min = num_1 - num_2;
 
-console.log("Minus: " + (num_1 - num_2));
+// console.log("Minus: " + (num_1 - num_2));
  
-var str1 = ("11");
-var str2 = ("Apple");
-console.log("Result: " + (str1 + str2));
-var str3 = Number("15");
-var str4 = Number("15");
-console.log("Result: " + (str3 + str4));
+// var str1 = ("11");
+// var str2 = ("Apple");
+// console.log("Result: " + (str1 + str2));
+// var str3 = Number("15");
+// var str4 = Number("15");
+// console.log("Result: " + (str3 + str4));
 
-console.log("Math: " + Math.PI);
-console.log("Math: " + Math.max(6, 10, 11, 25, 3, -2));
-console.log("Math: " + Math.min(6, 10, 11, 25, 3, -2));
-console.log("Math: " + Math.E);
+// console.log("Math: " + Math.PI);
+// console.log("Math: " + Math.max(6, 10, 11, 25, 3, -2));
+// console.log("Math: " + Math.min(6, 10, 11, 25, 3, -2));
+// console.log("Math: " + Math.E);
+
+var number = 8;
+
+if(number == 5) {
+    console.log("OK");
+}else if(number < 5) {
+    console.log("5!");
+}else if(number ==7) {
+    console.log(" 7!");
+}else if(number >= 15) {
+    console.log(" =>15!");
+}else {
+    console.log("Else");
+}
+
+
+var str = "word";
+
+switch(str){
+    case"1":
+    console.log("Varriable is 1");
+    break;
+    case"2":
+    console.log("Varriable is 2");
+    break;
+    case"word":
+    console.log("Varriable is word");
+    break;
+}
