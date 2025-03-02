44303dcbaadca3db8db0b9e466c227588c96b0fd
Soviskas <kisilovoleksandr48@gmail.com>
2025-03-02 12:22:36 +0200

    Update#2-2.03.2025-12:22

--- main.js
+++ main.js
@@ -7,9 +7,9 @@
 // var num;
 // num = 10;
 
-var num = 10;
-num = 12;
-console.log("Variable:" + num);
+// var num = 10;
+// num = 12;
+// console.log("Variable:" + num);
 
 // var number;
 // number = true;
@@ -17,4 +17,20 @@ console.log("Variable:" + num);
 // number = 8.8;
 // number = "Stroka";
 
+var num_1 = 5;
+var num_2 = 10;
+// var min = num_1 - num_2;
+
+console.log("Minus: " + (num_1 - num_2));
  
+var str1 = ("11");
+var str2 = ("Apple");
+console.log("Result: " + (str1 + str2));
+var str3 = Number("15");
+var str4 = Number("15");
+console.log("Result: " + (str3 + str4));
+
+console.log("Math: " + Math.PI);
+console.log("Math: " + Math.max(6, 10, 11, 25, 3, -2));
+console.log("Math: " + Math.min(6, 10, 11, 25, 3, -2));
+console.log("Math: " + Math.E);
