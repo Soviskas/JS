d99fdcd7b0ef370f0783da36e6ede40c30c922df
Soviskas <kisilovoleksandr48@gmail.com>
2025-03-03 18:30:07 +0200

    Update#3-03.03.2025-18:29

--- main.js
+++ main.js
@@ -68,4 +68,62 @@
 //     break;
 // }
 
-var arr = [5, 6, true, "stroka", 5.2, -100];
+////////03.03.2025-17:09
+
+// arr-Array-Масив даних
+
+// var arr = [5, 6, true, "stroka", 5.2, -100]; 
+// arr[3] = "Dick"
+// console.log(arr[3]);
+// console.log(arr.length);
+// var matrix = [
+//     [3, 5, 8], ["Xuy", 2.0], [-10, 21]
+// ];
+
+// matrix[1] [0] = 90;
+// console.log(matrix);
+
+
+////////////03.03.2025-18:29
+
+// for(var n = 0; n <= 10; n++) {
+//     console.log(n);
+// }
+
+// console.log("---------------------------------");
+
+// var k = 20;
+// while(k >= 10) {
+// console.log(k);
+// k--;
+// }
+
+// console.log("---------------------------------");
+
+// var p = 0;
+// do{
+// console.log(p);
+// p++;
+// }while(p < 50);
+
+// console.log("---------------------------------");
+
+// var a = 10;
+// for(a; a <= 40; a += 5){
+//     // if(a > 25)
+//     //     break;
+
+//     if(a % 2 == 0)
+//         continue;
+
+//     console.log(a);
+// }
+
+// console.log("---------------------------------");
+
+// var arr = [7, 3, 12, 8, 20, 51];
+
+// for(var l = 0; l < arr.length; l++){
+//     arr[l] *= 3;
+//     console.log("Element " + (l + 1) + ": " + arr[l]);
+// }
